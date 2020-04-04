d3f5ebf364e49920e10ea8b07bca0bd570987e36
midori.n <uruoilemon-techx@yahoo.co.jp>
2020-04-04 22:25:15 +0900

    コメント追加

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const readline = require('readline');
 const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({'input': rs, 'output': {} });
 const prefectureDataMap = new Map();    //key:都道府県、value：集計データオブジェクト
+// イベント駆動型プログラミング
+// ：あらかじめイベントが発生したときに実行される関数を設定しておいて、起こったイベントに応じて処理を行うこと
 rl.on('line', (linestring) => {
   // console.log(linestring);
   const columns = linestring.split(',');
   const year = parseInt(columns[0]);
   const prefecture = columns[1];
-  const popu = parseInt(columns[3]);
+  const popu = parseInt(columns[2]);
   if (year === 2010 || year === 2015) {
     let value = prefectureDataMap.get(prefecture);
     if (!value) {
@@ -35,11 +37,15 @@ rl.on('close', () => {
   for (let [key,value] of prefectureDataMap) {
     value.change = value.popu15 / value.popu10;
   }
+  // 並び替え。
   const rankingArray = Array.from(prefectureDataMap).sort((pair1,pair2) => {
     return pair2[1].change - pair1[1].change;
   });
+  // 表示用に文字列を変換。
+  // rankingArrayに格納されている、変化率順にソートされたデータについて、Map関数で文字列結合したものをrankingStringsに格納。
+  // rankingStringsを表示させて終了する
   const rankingStrings = Array.from(rankingArray).map(([key,value]) => {
-    return key + ':' + value.popu10 + '=>' + value.popu15 + '変化率：' + value.change;
+    return key + ':' + value.popu10 + '=> ' + value.popu15 + '：変化率：' + value.change;
   } );
   console.log(rankingStrings);
 })
